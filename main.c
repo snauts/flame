82c02db22481c132ef0431c9356746782b6ac155
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-08-19 11:26:00 +0300

    init VDP registers

--- main.c
+++ main.c
@@ -2,15 +2,30 @@
 
 #define HW_VER		0xa10001
 #define TMSS_ADDR	0xa14000
+#define VDP_CTRL	0xc00004
 
-void tmss(void) {
+static void tmss(void) {
     if ((BYTE(HW_VER) & 0xf) != 0) {
 	LONG(TMSS_ADDR) = 0x53454741; /* "SEGA" */
     }
 }
 
+const unsigned char VDP_regs[] = {
+    0x14, 0x74, 0x30, 0x00, 0x07, 0x78, 0x00, 0x00,
+    0x00, 0x00, 0x08, 0x00, 0x81, 0x3F, 0x00, 0x02,
+    0x01, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x80
+};
+
+static void init_VDP(void) {
+    char i;
+    for (i = 0; i < ARRAY_SIZE(VDP_regs); i++) {
+	WORD(VDP_CTRL) = 0x8000 | (i << 8) | VDP_regs[i];
+    }
+}
+
 void _start(void) {
     tmss();
+    init_VDP();
     for (;;) {
     }
 }
--- main.h
+++ main.h
@@ -1,3 +1,5 @@
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
+
 #define BYTE(x) (* (volatile unsigned char *) (x))
 #define WORD(x) (* (volatile unsigned short *) (x))
 #define LONG(x) (* (volatile unsigned long *) (x))
