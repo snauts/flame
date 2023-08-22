7e446beda59e925cfe7b5293b39cd6ed994b505b
Arturs Grebstelis <arturs@mikrotik.com>
2023-08-22 14:01:11 +0300

    shorter type names

--- main.c
+++ main.c
@@ -7,8 +7,9 @@
 
 #define VDP_VRAM_WRITE	0x40000000
 #define VDP_CRAM_WRITE	0xc0000000
+#define VDP_SRAM_WRITE	0x40000010
 
-static void addr_VDP(unsigned flags, unsigned short addr) {
+static void addr_VDP(u32 flags, u16 addr) {
     LONG(VDP_CTRL) = flags | ((addr & 0x3fff) << 16) | (addr >> 14);
 }
 
@@ -18,7 +19,7 @@ static void tmss(void) {
     }
 }
 
-const unsigned char VDP_regs[] = {
+const byte VDP_regs[] = {
     0x14, 0x74, 0x30, 0x00, 0x07, 0x78, 0x00, 0x00,
     0x00, 0x00, 0x08, 0x00, 0x81, 0x3F, 0x00, 0x02,
     0x01, 0x00, 0x00, 0xFF, 0xFF, 0x00, 0x00, 0x80
@@ -31,7 +32,7 @@ static void init_VDP(void) {
     }
 }
 
-static int is_vblank(void) {
+static u16 is_vblank(void) {
     return WORD(VDP_CTRL) & BIT(4);
 }
 
@@ -39,7 +40,11 @@ static void wait_for_vblank(void) {
     while (!is_vblank()) { }
 }
 
-static void update_palette(unsigned short *buf, int offset, int count) {
+static void wait_for_draw(void) {
+    while (is_vblank()) { }
+}
+
+static void update_palette(const u16 *buf, int offset, int count) {
     int i;
     addr_VDP(VDP_CRAM_WRITE, offset);
     for (i = 0; i < count; i++) {
--- main.h
+++ main.h
@@ -1,7 +1,11 @@
-#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
-
 #define BIT(x) (1 << (x))
 
-#define BYTE(x) (* (volatile unsigned char *) (x))
-#define WORD(x) (* (volatile unsigned short *) (x))
-#define LONG(x) (* (volatile unsigned long *) (x))
+#define ARRAY_SIZE(x) (sizeof(x) / sizeof(*(x)))
+
+typedef unsigned char byte;
+typedef unsigned short u16;
+typedef unsigned int u32;
+
+#define BYTE(x) (* (volatile byte *) (x))
+#define WORD(x) (* (volatile u16 *) (x))
+#define LONG(x) (* (volatile u32 *) (x))
