bde5d45207d60c4e106b6c8d6663cd718b66708a
Arturs Grebstelis <arturs@mikrotik.com>
2023-08-22 10:59:47 +0300

    update palette fn

--- main.c
+++ main.c
@@ -8,7 +8,7 @@
 #define VDP_VRAM_WRITE	0x40000000
 #define VDP_CRAM_WRITE	0xc0000000
 
-static void VDP_addr(unsigned flags, unsigned short addr) {
+static void addr_VDP(unsigned flags, unsigned short addr) {
     LONG(VDP_CTRL) = flags | ((addr & 0x3fff) << 16) | (addr >> 14);
 }
 
@@ -37,6 +37,14 @@ static int is_vblank(void) {
 
 static void wait_for_vblank(void) {
     while (!is_vblank()) { }
+}
+
+static void update_palette(unsigned short *buf, int offset, int count) {
+    int i;
+    addr_VDP(VDP_CRAM_WRITE, offset);
+    for (i = 0; i < count; i++) {
+	WORD(VDP_DATA) = buf[i];
+    }
 }
 
 void _start(void) {
