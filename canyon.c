bb4eb62343969dd00fc6d875d696fc05b9853e7f
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-08-23 22:32:18 +0300

    decrease ROM size by 100 bytes

--- canyon.c
+++ canyon.c
@@ -13,20 +13,25 @@ void paint_background(int x, int y, int w, int h, int i, int n) {
     }
 }
 
+static void paint_cloud(int x, int y, int i) {
+    paint_background(x, y, 8, 2, i, 6);
+}
+
 void display_canyon(void) {
     update_palette(canyon_palette, 0, ARRAY_SIZE(canyon_palette));
     update_tiles(canyon_tiles, 1, ARRAY_SIZE(canyon_tiles));
 
-    fill_VRAM(VRAM_PLANE_A + 0x000, 1, 0x800);
-    paint_background( 3, 5, 8, 2, 1, 6);
-    paint_background( 8, 8, 8, 2, 7, 6);
-    paint_background(12, 2, 8, 2, 3, 6);
-    paint_background(19, 7, 8, 2, 5, 6);
-    paint_background(24, 4, 8, 2, 7, 6);
-    paint_background(30, 8, 8, 2, 3, 6);
-    paint_background(36, 1, 8, 2, 1, 6);
-    paint_background(42, 6, 8, 2, 5, 6);
-    paint_background(47, 3, 8, 2, 7, 6);
-    paint_background(51, 7, 8, 2, 1, 6);
-    paint_background(56, 2, 8, 2, 3, 6);
+    fill_VRAM(VRAM_PLANE_A + 0x000,  1, 0x300);
+    fill_VRAM(VRAM_PLANE_A + 0x600, 65, 0x400);
+    paint_cloud( 3, 5, 1);
+    paint_cloud( 8, 8, 7);
+    paint_cloud(12, 2, 3);
+    paint_cloud(19, 7, 5);
+    paint_cloud(24, 4, 7);
+    paint_cloud(30, 8, 3);
+    paint_cloud(36, 1, 1);
+    paint_cloud(42, 6, 5);
+    paint_cloud(47, 3, 7);
+    paint_cloud(51, 7, 1);
+    paint_cloud(56, 2, 3);
 }
