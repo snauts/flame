ac98d0801d67d983d0bd71986db25caa64c05715
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-08-23 21:44:51 +0300

    fill sky with clouds

--- canyon.c
+++ canyon.c
@@ -18,8 +18,15 @@ void display_canyon(void) {
     update_tiles(canyon_tiles, 1, ARRAY_SIZE(canyon_tiles));
 
     fill_VRAM(VRAM_PLANE_A + 0x000, 1, 0x800);
-    paint_background( 5, 5, 8, 2, 1, 6);
-    paint_background(24, 4, 8, 2, 3, 6);
-    paint_background(20, 7, 8, 2, 5, 6);
-    paint_background(12, 2, 8, 2, 7, 6);
+    paint_background( 3, 5, 8, 2, 1, 6);
+    paint_background( 8, 8, 8, 2, 7, 6);
+    paint_background(12, 2, 8, 2, 3, 6);
+    paint_background(19, 7, 8, 2, 5, 6);
+    paint_background(24, 4, 8, 2, 7, 6);
+    paint_background(30, 8, 8, 2, 3, 6);
+    paint_background(36, 1, 8, 2, 1, 6);
+    paint_background(42, 6, 8, 2, 5, 6);
+    paint_background(47, 3, 8, 2, 7, 6);
+    paint_background(51, 7, 8, 2, 1, 6);
+    paint_background(56, 2, 8, 2, 3, 6);
 }
