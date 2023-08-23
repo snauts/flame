21c2738d3f4cee642fdae17696c2b0b329c5c34e
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-08-23 23:35:19 +0300

    small hills on horizon

--- canyon.c
+++ canyon.c
@@ -13,8 +13,40 @@ void paint_background(int x, int y, int w, int h, int i, int n) {
     }
 }
 
+const byte cloud_data[] = {
+    3,  5, 1,
+    8,  8, 7,
+    12, 2, 3,
+    19, 7, 5,
+    24, 4, 7,
+    30, 8, 3,
+    36, 1, 1,
+    42, 6, 5,
+    47, 3, 7,
+    51, 7, 1,
+    56, 2, 3,
+};
+
 static void paint_cloud(int x, int y, int i) {
     paint_background(x, y, 8, 2, i, 6);
+}
+
+static void draw_clouds(void) {
+    int i;
+    for (i = 0; i < ARRAY_SIZE(cloud_data); i += 3) {
+	paint_cloud(cloud_data[i + 0], cloud_data[i + 1], cloud_data[i + 2]);
+    }
+}
+
+const byte horizon_data[] = {
+    65, 66, 67, 68, 67, 66, 65, 67, 65, 68, 66, 68, 65, 67, 68, 66
+};
+
+static void draw_horizon(void) {
+    int x, i = 0;
+    for (x = 0; x <= 60; x += 4) {
+	paint_background(x, 12, 4, 1, horizon_data[i++], 7);
+    }
 }
 
 void display_canyon(void) {
@@ -22,16 +54,8 @@ void display_canyon(void) {
     update_tiles(canyon_tiles, 1, ARRAY_SIZE(canyon_tiles));
 
     fill_VRAM(VRAM_PLANE_A + 0x000,  1, 0x300);
-    fill_VRAM(VRAM_PLANE_A + 0x600, 65, 0x400);
-    paint_cloud( 3, 5, 1);
-    paint_cloud( 8, 8, 7);
-    paint_cloud(12, 2, 3);
-    paint_cloud(19, 7, 5);
-    paint_cloud(24, 4, 7);
-    paint_cloud(30, 8, 3);
-    paint_cloud(36, 1, 1);
-    paint_cloud(42, 6, 5);
-    paint_cloud(47, 3, 7);
-    paint_cloud(51, 7, 1);
-    paint_cloud(56, 2, 3);
+    fill_VRAM(VRAM_PLANE_A + 0x600, 69, 0x400);
+
+    draw_clouds();
+    draw_horizon();
 }
