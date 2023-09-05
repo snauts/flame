423be0f4a2a9e0ff47b66e6de3d917b331108c67
Arturs Grebstelis <arturs@mikrotik.com>
2023-09-05 12:00:27 +0300

    z80 subroutine to write ym2612 registers

--- z80.asm
+++ z80.asm
@@ -13,8 +13,23 @@ vblank:
 	call	tick
 	ei
 	reti
+
 init:
 	ret
+
 tick:
+	ret
+
+ym2612_write:
+	;; a - part, b - addr, c - data
+	sla	a
+	ld	ixl, a
+	ld	ixh, 0x40
+1$:	ld	a, (ix)
+	and	a, 0x80
+	jp	nz, 1$
+	ld	(ix+0), b
+	nop
+	ld	(ix+1), c
 	ret
 	align	2
