16e66c7155c2e98b449a6a950da6a3dd7123e66d
Arturs Grebstelis <arturs@mikrotik.com>
2023-09-05 17:33:24 +0300

    z80 plays music once on its own

--- z80.asm
+++ z80.asm
@@ -34,6 +34,8 @@ tick:
 
 ym2612_write:
 	;; a - part, b - addr, c - data
+	push	af
+	sla	a
 	sla	a
 	ld	ixl, a
 	ld	ixh, 0x40
@@ -43,9 +45,62 @@ ym2612_write:
 	ld	(ix+0), b
 	nop
 	ld	(ix+1), c
+	pop	af
+	ret
+
+ym2612_note:
+	;; a - channel, de - note
+	push	af
+	ld	c, a
+	sra	a
+	sra	a
+	ld	b, 0x28
+	call	ym2612_write
+
+	push	af
+	ld	a, c
+	and	a, 3
+	add	a, 0xa4
+	ld	b, a
+	pop	af
+	ld	c, e
+	call	ym2612_write
+
+	push	af
+	ld	a, b
+	sub	a, 4
+	ld	b, a
+	pop	af
+	ld	c, d
+	call	ym2612_write
+
+	pop	af
+	and	a, 3
+	or	a, 0xf0
+	ld	c, a
+	ld	b, 0x28
+	call	ym2612_write
 	ret
 
 play_note:
+	ld	hl, (next)
+	ld	a, (hl)
+	and	a
+	ret	z
+	inc	hl
+
+	ld	de, (hl)
+	inc	hl
+	inc 	hl
+
+	call	ym2612_note
+
+	ld	a, (hl)
+	inc	hl
+
+	ld	(wait), a
+	ld	(next), hl
+
 	ret
 
 	align	2
