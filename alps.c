9e6f5fffb4d37603b394c1547fcda7d777406b38
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-10-18 13:27:42 +0300

    paint mountains over skyline

--- alps.c
+++ alps.c
@@ -2,6 +2,31 @@
 
 #include "images/alps.h"
 
+static u16 draw_one_mountain(u16 x, byte tile) {
+    if (tile == 1 || tile == 33) {
+	paint_background(x, 10, 4, 2, tile, 6);
+	x += 4;
+    }
+    else {
+	/* 3, 19, 35, 51 */
+	paint_background(x, 11, 2, 1, tile, 7);
+	x += 2;
+    }
+    return x;
+}
+
+static const byte mountain_tiles[] = {
+    1, 3, 19, 51, 33, 1, 35, 33, 51, 35, 3, 1,
+    19, 35, 33, 3, 35, 1, 51, 19, 33, 19, 3, 51,
+};
+
+static void draw_mountains(void) {
+    u16 x = 0, i = 0;
+    while (x < 64) {
+	x = draw_one_mountain(x, mountain_tiles[i++]);
+    }
+}
+
 void display_mountains(void) {
     /* load tiles */
     update_palette(alps_palette, 0, ARRAY_SIZE(alps_palette));
@@ -14,6 +39,9 @@ void display_mountains(void) {
     /* background */
     fill_VRAM(0x000, 12, 0x300);
     fill_VRAM(0x600,  4, 0x500);
+
+    draw_mountains();
+
     copy_to_VRAM(VRAM_PLANE_B, DMA_BUF_SIZE);
 
     /* foreground */
