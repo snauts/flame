5afe846eaa9833ce391b951d27d0f80232a872d7
Arturs Grebstelis <arturs@mikrotik.com>
2023-10-23 16:05:34 +0300

    add rock platform sides

--- alps.lisp
+++ alps.lisp
@@ -1,5 +1,5 @@
 (defparameter *alps-walkable*
-  '(67 75 83 91 99 107 115 123))
+  '(67 75 83 91 99 107 115 123 71 79))
 
 (defun alpine-tile (id)
   (tile id :pl 1))
@@ -8,9 +8,14 @@
   (fill-box 8 8 (alpine-tile 65)))
 
 (defun alps-walk (&optional (n 1))
-  (multiply (crop 0 4 8 8 (rocks)) n))
+  (join
+   (crop 0 0 1 3 (rocks))
+   (multiply (crop 0 4 8 8 (rocks)) n)
+   (crop 1 0 2 3 (rocks))))
 
 (defun mountain-level ()
   (join
+   (alps-walk 3)
+   (empty 4)
    (alps-walk 16)
    (empty 64)))
