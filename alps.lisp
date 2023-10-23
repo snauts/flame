f9ef9d89e928095710ba1deb051a6426f4b9d2ce
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-10-23 21:13:13 +0300

    two leaf plant and larger three leaf plant

--- alps.lisp
+++ alps.lisp
@@ -12,11 +12,15 @@
 (defun two-leaf ()
   (crop 0 3 2 4 (rocks)))
 
+(defun small-plant ()
+  (crop 2 2 6 4 (rocks)))
+
 (defun alpine-rocks ()
   (let ((rocks (crop 0 4 8 8 (rocks))))
     (case *rock-type*
       (0 rocks)
-      (1 (place 3 3 rocks (two-leaf))))))
+      (1 (place 2 3 rocks (two-leaf)))
+      (2 (place 4 3 rocks (small-plant))))))
 
 (defun alps-walk (&key (width 1) (type 0))
   (let ((*rock-type* type))
@@ -31,5 +35,7 @@
    (empty 4)
    (alps-walk :width 1 :type 1)
    (empty 2)
-   (alps-walk :width 11)
+   (alps-walk :width 1 :type 2)
+   (empty 2)
+   (alps-walk :width 8)
    (empty 64)))
