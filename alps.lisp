9a587a665e75a54008c3a5884905b5f94a16e849
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2023-10-25 23:50:31 +0300

    add primitive for combined flowers

--- alps.lisp
+++ alps.lisp
@@ -18,6 +18,11 @@
 (defun flower-bush ()
   (crop 2 0 6 2 (rocks)))
 
+(defun flowers-and-plant (rocks)
+  (box-pipe
+   (place 4 3 rocks (small-plant))
+   (place 0 3 pipe (flower-bush))))
+
 (defun alpine-rocks ()
   (let ((rocks (crop 0 4 8 8 (rocks))))
     (case *rock-type*
@@ -25,7 +30,8 @@
       (1 (place 2 3 rocks (two-leaf)))
       (2 (place 4 3 rocks (small-plant)))
       (3 (place 0 3 rocks (flower-bush)))
-      (4 (crop 6 0 8 4 (rocks))))))
+      (4 (flowers-and-plant rocks))
+      (5 (crop 6 0 8 4 (rocks))))))
 
 (defun alps-walk (&key (width 1) (type 0))
   (let ((*rock-type* type))
@@ -37,7 +43,7 @@
 (defun mountain-level ()
   (join
    (alps-walk :width 3)
-   (empty 4)
+   (empty 2)
    (alps-walk :width 1 :type 1)
    (empty 2)
    (alps-walk :width 1 :type 2)
@@ -47,5 +53,5 @@
    (empty 2)
    (alps-walk :width 1 :type 4)
    (empty 2)
-   (alps-walk :width 2)
+   (alps-walk :width 1 :type 5)
    (empty 64)))
