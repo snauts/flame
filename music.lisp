29bc64488f6ee3f625d1785a77cb108ca6730729
Arturs Grebstelis <arturs@mikrotik.com>
2023-11-01 12:50:57 +0200

    fix musical note B index

--- music.lisp
+++ music.lisp
@@ -1,7 +1,7 @@
 (defparameter *notes*
   '((C  0) (Cs 1) (D  2)  (Ds 3)
     (E  4) (F  5) (Fs 6)  (G  7)
-    (Gs 8) (A  9) (As 10) (B 10)))
+    (Gs 8) (A  9) (As 10) (B 11)))
 
 (defparameter *johnny*
   '((2 (0 0 D))
