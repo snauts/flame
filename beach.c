75eeb38d72a191f42d8d0c80ea8dfa5807aed850
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-02-14 17:23:58 +0200

    fix beach level PLANE_A init order

--- beach.c
+++ beach.c
@@ -122,9 +122,8 @@ static void display_nippon(Function prepare_level) {
     /* foreground */
     clear_DMA_buffer(0, 0x1000);
 
-    prepare_level();
-
     fill_VRAM(0, 0, 0x800);
+    prepare_level();
     fill_bottom_row();
     copy_to_VRAM(VRAM_PLANE_A, DMA_BUF_SIZE);
 
