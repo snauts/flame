73b7777ff020ecd4499b6c20d1be3ca3e58fd81e
Arturs Grebstelis <arturs@mikrotik.com>
2024-02-14 13:17:06 +0200

    use unused transparent tile for dummy walkway

--- beach.lisp
+++ beach.lisp
@@ -1,8 +1,8 @@
 (defparameter *beach-walkable*
-  '(100))
+  '(300))
 
 (defun beach-walk (n)
-  (multiply (stack (cell 100) 2) n))
+  (multiply (stack (cell 300) 2) n))
 
 (defun beach-level ()
   (beach-walk 256))
