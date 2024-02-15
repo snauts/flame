4dc05188844c6b461f6f8454e4ead57b501c0b49
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-02-15 21:13:45 +0200

    add dune platform primitives - bamboo holes

--- beach.lisp
+++ beach.lisp
@@ -1,5 +1,6 @@
 (defparameter *beach-walkable*
-  '(130 138 146 154 162 170 178 186 132 140))
+  '(130 138 146 154 162 170 178 186
+    132 140 148 156 164 172 180 188))
 
 (defun beach-tile (id)
   (tile id :pl 1))
@@ -7,25 +8,37 @@
 (defun beach-rocks (x1 y1 x2 y2)
   (crop x1 y1 x2 y2 (fill-box 16 8 (beach-tile 129))))
 
-(defun dune-right-end ()
-  (beach-rocks 0 4 1 6))
+(defun dune-R ()
+  (beach-rocks 4 4 6 6))
 
-(defun dune-left-end ()
-  (beach-rocks 1 4 2 6))
+(defun dune-L ()
+  (beach-rocks 6 4 8 6))
 
-(defun beach-dune ()
+(defun dune-2 ()
+  (beach-rocks 0 4 2 6))
+
+(defun dune-4 ()
+  (join (beach-rocks 0 6 2 8) (beach-rocks 6 6 8 8)))
+
+(defun dune-8 ()
   (beach-rocks 0 6 8 8))
 
-(defun dune-platform (&key (width 1))
-  (join (dune-left-end)
-	(multiply (beach-dune) width)
-	(dune-right-end)))
+(defun beach-dune (type)
+  (case type
+    (2 (dune-2))
+    (4 (dune-4))
+    (8 (dune-8))))
+
+(defun dune-platform (&key (width 1) (type 8))
+  (join (dune-L) (multiply (beach-dune type) width) (dune-R)))
 
 (defun beach-level ()
   (join
    (dune-platform :width 2)
    (empty 2)
-   (dune-platform :width 2)
+   (dune-platform :width 2 :type 2)
+   (empty 2)
+   (dune-platform :width 2 :type 4)
    (empty 2)
    (dune-platform :width 10)
    (empty 48)))
