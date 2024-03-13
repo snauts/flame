da27428a71c7a5bdfd03c9beac2ad4b8a523a772
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-03-13 23:45:26 +0200

    add brick walkway endings

--- town.lisp
+++ town.lisp
@@ -1,7 +1,7 @@
 (load "level.lisp")
 
 (defparameter *town-walkable*
-  '(130 138 146 154))
+  '(130 138 146 154 162 170))
 
 (defun street-tile (id &key (v 0) (h 0) (pr 0))
   (tile id :pl 1 :v v :h h :pr pr))
@@ -12,11 +12,14 @@
 (defun street (x1 y1 x2 y2)
   (crop x1 y1 x2 y2 (fill-box 16 8 (street-tile 129))))
 
+(defun walk-middle (n)
+  (multiply (street 1 5 5 8) n))
+
 (defun town-walk (n)
-  (multiply (street 0 5 4 8) n))
+  (join (street 0 5 1 8) (walk-middle n) (street 5 5 6 8)))
 
 (defun town-level ()
-  (join (town-walk 32) (empty 64)))
+  (join (multiply (join (town-walk 4) (empty 2)) 4) (empty 64)))
 
 (defun commit-save ()
   (push-level "town_level" (town-level))
