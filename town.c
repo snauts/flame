f955f46d14750b3175cb925bf285906a0ca408e8
Arturs Grebstelis <arturs@mikrotik.com>
2024-04-08 11:07:31 +0300

    play sfx when bricks are being knocked out

--- town.c
+++ town.c
@@ -787,6 +787,7 @@ static void select_window_pattern(Object *obj) {
 static void king_break_out(u16 stage) {
     if (stage <= 2) {
 	set_mob_order(-1);
+	play_sfx(SFX_PERISH);
 	callback(&king_break_out, 50, stage + 1);
 	if (stage == 0) {
 	    window_damage(crown->direction);
