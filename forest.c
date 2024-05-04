30a42a5caf03dcb7f202cf70f4526f36ebe0f6cb
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-05-04 22:26:07 +0300

    update rain cloud layer density

--- forest.c
+++ forest.c
@@ -8,16 +8,22 @@ static void sky_piece(u16 x, u16 y, u16 dx, u16 dy) {
     paint_background(x, y, 2, 1, dx * 14 + dy + 1, 6);
 }
 
+static const byte sky_layers[] = {
+    0, 0, 1, 2, 2, 3, 4, 4, 5, 6, 6, 6
+};
+
 static void draw_sky(void) {
-    u16 i = 0;
-    for (u16 x = 0; x < 64; x += 2) {
-	sky_piece(x, 0, i, 0);
-	i = (i + (random() % 3) + 1) & 3;
+    for (u16 y = 0; y < ARRAY_SIZE(sky_layers); y++) {
+	u16 i = y;
+	set_seed(2077);
+	for (u16 x = 0; x < 64; x += 2) {
+	    sky_piece(x, y, i, sky_layers[y]);
+	    i = (i + (random() % 3) + 1) & 3;
+	}
     }
 }
 
 static void display_soviet(const Level *level) {
-    set_seed(1905);
     load_soldier_tiles(4);
 
     load_image(&rain_img, 1, 0);
