de308454c117c0c4e1be7ac7665f05414c3138f8
Arturs Grebstelis <arturs.grebstelis@gmail.com>
2024-05-13 01:24:02 +0300

    add bridge made from pine wood logs and platforms

--- forest.lisp
+++ forest.lisp
@@ -2,7 +2,8 @@
 
 (defparameter *forest-walkable*
   '(139 147 155 163 171 179 187 195 203 211
-    143 151 159 167 175 183 191 199))
+    143 151 159 167 175 183 191 199
+    214 222 230 238 144))
 
 (defun forest-tile (id &key (v 0) (h 0) (pr 0))
   (tile id :pl 1 :v v :h h :pr pr))
@@ -48,19 +49,34 @@
   (cond ((= n 1) (forest-cell 217))
 	((> n 1) (on-top (log-segment n) (forest-log (1- n))))))
 
+(defun forest-platform (n)
+  (join (mud 8 2 10 4) (multiply (mud 10 2 12 4) n) (mud 12 2 14 4)))
+
+(defun bridge-front (h)
+  (place 0 h (forward (forest-log (+ h 4))) (forest-cell 144)))
+
+(defun forest-bridge (n h)
+  (let ((w (* 2 n)))
+    (s-push (forest-log (+ h 5)))
+    (s-place (- w 4) 0 (forest-log (+ h 5)))
+    (s-place -2 h (forest-platform n))
+    (s-place 4 0 (bridge-front h))
+    (s-place w 0 (bridge-front h))
+    (s-pop)))
+
 (defun forest-level ()
   (join (forest-walk 2)
 	(forest-puddle)
 	(forest-walk)
 	(platform-end-L)
-	(empty 1)
-	(forest-log 10)
-	(empty 1)
+	(empty 3)
 	(platform-end-R)
 	(forest-walk-config :top-L 2 :top-R 1 :body 0)
 	(forest-walk-config :top-L 0 :top-R 3 :body 0)
 	(forest-walk-config :body 1)
 	(platform-end-L)
+	(empty 2)
+	(forest-bridge 8 2)
 	(empty 64)))
 
 (defun commit-save ()
